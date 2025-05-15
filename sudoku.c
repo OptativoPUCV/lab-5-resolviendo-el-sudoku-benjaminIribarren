28ea6bd02013c90eec75fb8f33df50ce996a2941
Your Name <you@example.com>
2025-05-15 15:47:33 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -112,7 +112,7 @@ int is_final(Node* n){
     for(int j = 0; j < 9; j++)
       if(n->sudo[i][j] == 0)
         return 0;  
-  return NULL; 
+  return -1; 
 }
 
 Node* DFS(Node* inicial, int* contador){
