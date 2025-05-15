2ee01061054244625bceb07120ba1c70f4f2b1ca
Your Name <you@example.com>
2025-05-15 13:15:48 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -110,8 +110,10 @@ List* get_adj_nodes(Node* n){
 int is_final(Node* n){
   for(int i = 0; i < 9; i++)
     for(int j = 0; j < 9; j++)
-      if(n->sudo[i][j] == 0) return 0;
-  return is_valid(n);
+      if(n->sudo[i][j] == 0)
+        return 0;  // aún hay celdas vacías
+
+  return is_valid(n); // si está lleno, debe ser válido
 }
 
 Node* DFS(Node* inicial, int* contador){
